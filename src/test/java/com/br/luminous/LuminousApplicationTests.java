8dc33bfe9bb4eab43d0b85667d94d48b512fdd4d
LuizFJP <luizfjportela@gmail.com>
2023-03-07 23:13:03 -0300

    remove: method default in tests

--- src/test/java/com/br/luminous/LuminousApplicationTests.java
+++ src/test/java/com/br/luminous/LuminousApplicationTests.java
@@ -4,10 +4,4 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LuminousApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+class LuminousApplicationTests {}
