db2a0414e832cbf2dc5a6aa79faf9bd529c41c3a
LuizFJP <luizfjportela@gmail.com>
2023-03-09 01:21:03 -0300

    fix: api

--- src/main/java/com/br/luminous/repository/UserRepository.java
+++ src/main/java/com/br/luminous/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.br.luminous.repository;
 
 import com.br.luminous.entity.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
 }
--- src/main/java/com/br/luminous/service/UserService.java
+++ src/main/java/com/br/luminous/service/UserService.java
@@ -3,7 +3,6 @@ package com.br.luminous.service;
 import com.br.luminous.entity.User;
 import com.br.luminous.repository.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
