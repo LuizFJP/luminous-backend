451c66765ead5d36f32f51e7ef19f6a125c87a3c
Nicolas D <78985734+Deb4cker@users.noreply.github.com>
2023-03-10 19:33:00 -0300

    testing api

--- src/main/java/com/br/luminous/controller/UserController.java
+++ src/main/java/com/br/luminous/controller/UserController.java
@@ -5,16 +5,18 @@ import com.br.luminous.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/user")
 public class UserController {
     private UserService userService;
+
+    @RequestMapping(path = "", method = RequestMethod.GET)
+    public ResponseEntity<String> teste() {
+        return new ResponseEntity<String>("Deu certo", HttpStatus.CREATED);
+    }
 
     @PostMapping("/")
     public ResponseEntity<Long> createUser(@RequestBody User user) {
