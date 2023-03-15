46ad4d8c2b1159fda83618056c3bcc1836bf5442
Nicolas D <78985734+Deb4cker@users.noreply.github.com>
2023-03-14 21:49:30 -0300

    crud device start

--- src/main/java/com/br/luminous/entity/Device.java
+++ src/main/java/com/br/luminous/entity/Device.java
@@ -1,3 +1,12 @@
 package com.br.luminous.entity;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class Device {
+
+    private String name;
+    private float power;
+    private String usage_time;
+    private String category;
+
 }
