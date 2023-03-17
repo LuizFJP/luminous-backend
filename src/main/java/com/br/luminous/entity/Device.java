c37c60bbf1b66dba14a45f4a051e36814188235f
Nicolas D <78985734+Deb4cker@users.noreply.github.com>
2023-03-17 19:41:12 -0300

    user Request working

--- src/main/java/com/br/luminous/entity/Device.java
+++ src/main/java/com/br/luminous/entity/Device.java
@@ -1,9 +1,14 @@
 package com.br.luminous.entity;
-import lombok.AllArgsConstructor;
+import lombok.Data;
+import javax.persistence.*;
 
-@AllArgsConstructor
+@Entity
+@Data
+@Table(name ="devices")
 public class Device {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
     private String name;
     private float power;
     private String usage_time;
