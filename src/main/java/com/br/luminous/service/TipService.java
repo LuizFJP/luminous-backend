501f1cedab2577eb350d9a4fd7049b9bce3bab5e
gusdev0258 <16326176760@edu.udesc.br>
2023-04-27 16:17:15 -0300

    add: tip entity

--- src/main/java/com/br/luminous/service/TipService.java
+++ src/main/java/com/br/luminous/service/TipService.java
@@ -3,9 +3,10 @@ package com.br.luminous.service;
 import com.br.luminous.entity.Tip;
 import com.br.luminous.exceptions.TipNotFoundException;
 import com.br.luminous.repository.TipRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-
+@Service
 public class TipService {
     TipRepository tipRepository;
     public Tip getTip(Long id) {
