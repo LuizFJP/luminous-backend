a604f1725e2beaa959b71246b00be0edae11d634
gusdev0258 <16326176760@edu.udesc.br>
2023-05-22 19:17:55 -0300

    feat: refactored EnergyBillController to fill the restful pattern

--- src/main/java/com/br/luminous/controller/EnergyBillController.java
+++ src/main/java/com/br/luminous/controller/EnergyBillController.java
@@ -1,8 +1,13 @@
 package com.br.luminous.controller;
 
 
+import com.br.luminous.exceptions.AddressNotFoundException;
+import com.br.luminous.exceptions.BillFileNotFoundException;
+import com.br.luminous.exceptions.EnergyBillNotFoundException;
+import com.br.luminous.models.ApiResponse;
 import com.br.luminous.models.EnergyBillRequest;
 import com.br.luminous.models.EnergyBillResponse;
+import com.br.luminous.service.ApiResponseService;
 import com.br.luminous.service.EnergyBillService;
 import lombok.AllArgsConstructor;
 import org.apache.coyote.Response;
@@ -21,31 +26,60 @@ import java.util.List;
 public class EnergyBillController {
     private EnergyBillService energyBillService;
 
+    private ApiResponseService apiResponseService;
     @GetMapping("/{id}")
-    public ResponseEntity<EnergyBillResponse> getEnergyBillById(@PathVariable Long id){
-        EnergyBillResponse energyBillResponse = energyBillService.getById(id);
-        return new ResponseEntity<EnergyBillResponse>(energyBillResponse, HttpStatus.OK);
+    public ResponseEntity<ApiResponse<EnergyBillResponse>> getEnergyBillById(@PathVariable Long id) {
+        try {
+            EnergyBillResponse energyBillResponse = energyBillService.getById(id);
+            ApiResponse<EnergyBillResponse> response = apiResponseService.createSuccessResponse(energyBillResponse, "Energy bill for id: "+ id + "was successfully returned");
+            return ResponseEntity.ok(response);
+        } catch (EnergyBillNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiResponseService.createErrorResponse("Energy Bill was not found for id: " + id));
+        }
     }
-        @PostMapping("/address/{address_id}/billFile/{billFile_id}")
-    public ResponseEntity<Long> createEnergyBill(@PathVariable Long address_id,@PathVariable Long billFile_id,@RequestBody EnergyBillRequest energyBillRequest){
+
+    @PostMapping("/address/{address_id}/billFile/{billFile_id}")
+    public ResponseEntity<ApiResponse<Long>> createEnergyBill(@PathVariable Long address_id, @PathVariable Long billFile_id, @RequestBody EnergyBillRequest energyBillRequest) {
+        try {
             Long id = energyBillService.create(address_id, billFile_id, energyBillRequest);
-            return new ResponseEntity<Long>(id, HttpStatus.CREATED);
+            ApiResponse<Long> response = apiResponseService.createSuccessResponse(id, "Energy Bill was created successfully");
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (AddressNotFoundException | BillFileNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiResponseService.createErrorResponse(e.getMessage()));
         }
-        @GetMapping("/getAll/{address_id}")
-    public ResponseEntity<List<EnergyBillResponse>> getAll(@PathVariable Long address_id){
-        var response = energyBillService.getAll(address_id);
-        return new ResponseEntity<List<EnergyBillResponse>>(
-                response, HttpStatus.OK);
+    }
+
+    @GetMapping("/getAll/{address_id}")
+    public ResponseEntity<ApiResponse<List<EnergyBillResponse>>> getAll(@PathVariable Long address_id) {
+        try {
+            List<EnergyBillResponse> response = energyBillService.getAll(address_id);
+            ApiResponse<List<EnergyBillResponse>> apiResponse = apiResponseService.createSuccessResponse(response, "All Energy Bill for Address id: " + address_id + "was successfully returned");
+            return ResponseEntity.ok(apiResponse);
+        }catch(AddressNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiResponseService.createErrorResponse(e.getMessage()));
         }
-        @PutMapping("/{id}")
-    public ResponseEntity<EnergyBillResponse> update(@PathVariable Long id, @RequestBody EnergyBillRequest energyBillRequest){
-            var response = energyBillService.update(id, energyBillRequest);
-            return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<EnergyBillResponse>> update(@PathVariable Long id, @RequestBody EnergyBillRequest energyBillRequest) {
+        try {
+            EnergyBillResponse response = energyBillService.update(id, energyBillRequest);
+            ApiResponse<EnergyBillResponse> apiResponse = apiResponseService.createSuccessResponse(response, "Energy Bill for id: " + id + "was successfully updated");
+            return ResponseEntity.ok(apiResponse);
+        } catch (EnergyBillNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiResponseService.createErrorResponse("Energy Bill was not found for id: " + id));
         }
-        @DeleteMapping("/{id}")
-    public ResponseEntity<EnergyBillResponse> delete(@PathVariable Long id){
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<Void>> delete(@PathVariable Long id) {
+        try {
             energyBillService.delete(id);
-            return new ResponseEntity(HttpStatus.OK);
+            ApiResponse<Void> response = apiResponseService.createSuccessResponse(null, "Energy bill for id: " + id + "was successfully deleted");
+            return ResponseEntity.ok(response);
+        }catch(EnergyBillNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiResponseService.createErrorResponse(e.getMessage()));
         }
+    }
 
 }
