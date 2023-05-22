1f78a86d5ff3f0b5d4c14ae11274f9af96fee863
gusdev0258 <16326176760@edu.udesc.br>
2023-05-22 09:09:32 -0300

    feat: created CRUD of ConsumptionAlerts

--- src/main/java/com/br/luminous/controller/ConsumptionAlertController.java
+++ src/main/java/com/br/luminous/controller/ConsumptionAlertController.java
@@ -1,12 +1,48 @@
 package com.br.luminous.controller;
 
+import com.br.luminous.entity.ConsumptionAlert;
+import com.br.luminous.service.ConsumptionAlertService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/consumption-alert")
 public class ConsumptionAlertController {
+    ConsumptionAlertService consumptionAlertService;
+    @PostMapping("/user/{id}")
+    public ResponseEntity<Long> create(@PathVariable Long user_id, @RequestBody ConsumptionAlert consumptionAlert){
+        Long id = consumptionAlertService.create(user_id, consumptionAlert);
+        return new ResponseEntity<>(id, HttpStatus.CREATED);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<ConsumptionAlert> update(@PathVariable Long id, @RequestBody ConsumptionAlert consumptionAlert){
+        var response = consumptionAlertService.update(id, consumptionAlert);
+        return new ResponseEntity(response, HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ConsumptionAlert> delete(@PathVariable Long id){
+        consumptionAlertService.delete(id);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<ConsumptionAlert> getConsumptionAlert(@PathVariable Long id){
+        var response = consumptionAlertService.get(id);
+        if(response != null){
+            return new ResponseEntity<ConsumptionAlert>(response, HttpStatus.OK);
+        }
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    @GetMapping("/getAll/user/{id}")
+    public ResponseEntity<List<ConsumptionAlert>> getAll(@PathVariable Long user_id){
+        var response = consumptionAlertService.getAll(user_id);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/br/luminous/service/ConsumptionAlertService.java
+++ src/main/java/com/br/luminous/service/ConsumptionAlertService.java
@@ -1,29 +1,57 @@
 package com.br.luminous.service;
 
+import com.br.luminous.controller.ConsumptionAlertController;
 import com.br.luminous.entity.ConsumptionAlert;
+import com.br.luminous.exceptions.ConsumptionAlertNotFound;
 import com.br.luminous.exceptions.UserNotFoundException;
 import com.br.luminous.repository.ConsumptionAlertRepository;
 import com.br.luminous.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class ConsumptionAlertService {
 
     private ConsumptionAlertRepository consumptionAlertRepository;
+    private UserService userService;
     private UserRepository userRepository;
 
     public Long create(Long user_id, ConsumptionAlert consumptionAlert){
         try {
-            var user = userRepository.findById(user_id).get();
+            var user = userService.getUserById(user_id);
             var consumptionAlertsList = user.getConsumptionAlerts();
             consumptionAlertsList.add(consumptionAlert);
             user.setConsumptionAlerts(consumptionAlertsList);
             userRepository.save(user);
             var consumptionAlertPersisted = consumptionAlertRepository.save(consumptionAlert);
             return consumptionAlertPersisted.getId();
-        }catch(UserNotFoundException e){
+        }catch(Exception e){
+            throw e;
+        }
+    }
+    public ConsumptionAlert get(Long id){
+        var response = consumptionAlertRepository.findById(id);
+        return response.orElseThrow(ConsumptionAlertNotFound::new);
+    }
+    public ConsumptionAlert update(Long id, ConsumptionAlert consumptionAlert){
+        var consumptionAlertToUpdate = consumptionAlertRepository.findById(id).get();
+        consumptionAlertToUpdate.setConsumptionLimit(consumptionAlert.getConsumptionLimit());
+        consumptionAlertToUpdate.setDescricao(consumptionAlert.getDescricao());
+        var response = consumptionAlertRepository.save(consumptionAlertToUpdate);
+        return response;
+    }
+    public List<ConsumptionAlert> getAll(Long user_id){
+        var user = userService.getUserById(user_id);
+        var consumptionAlertList = user.getConsumptionAlerts();
+        return consumptionAlertList;
+    }
+    public void delete(Long consumptionAlert_id){
+        try{
+            consumptionAlertRepository.deleteById(consumptionAlert_id);
+        }catch(ConsumptionAlertNotFound e){
             throw e;
         }
     }
