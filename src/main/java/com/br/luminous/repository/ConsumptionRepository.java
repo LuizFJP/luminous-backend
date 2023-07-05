40913c1427e615cfffda2ea80089bc32d3af3811
solange-d <solange.duemes@edu.udesc.br>
2023-07-04 22:46:10 -0300

    Alterada query ReportConsumption

--- src/main/java/com/br/luminous/repository/ConsumptionRepository.java
+++ src/main/java/com/br/luminous/repository/ConsumptionRepository.java
@@ -20,14 +20,15 @@ public interface ConsumptionRepository extends JpaRepository<Consumption, Long> 
     List<TrackProjection> getCurrentConsumption(Long addressId);
 
 
-    @Query(nativeQuery = true, value =
-            "select " +
-                    "name name, SUM(energy_consumptionKWh) energyConsumptionKWh," +
-                    " SUM(energy_consumption_reais) energyConsumptionReais, DATE_TRUNC('month', period) period" +
-            " FROM device " +
-            " INNER JOIN consumption " +
-            " ON consumption.device_id = device.id" +
-            " GROUP BY device.name, period" +
-            " ORDER BY period, energyConsumptionReais DESC, energyConsumptionKWh DESC")
+     @Query(nativeQuery = true, value =
+            "SELECT "+
+                    "name,DATE_TRUNC('month', period) period,"+
+            " SUM(energy_consumptionkwh) energyConsumptionKWh,"+
+            " SUM(energy_consumption_reais) energyConsumptionReais"+
+            " FROM device"+
+            " INNER JOIN consumption ON consumption.device_id = device.id"+
+            " GROUP BY name, DATE_TRUNC('month', period)"+
+            " ORDER BY DATE_TRUNC('month', period)"
+    )
     List<ReportProjection> getReport(Long addressId);
 }
